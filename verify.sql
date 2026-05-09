6b0619b8e8a5dc232d48200973a284e5a2c42972
Josue <josueworkflow@gmail.com>
2026-05-09 16:27:33 -0300

    chore: removendo arquivos temporários e sensíveis

--- verify.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'pedidos' as tabela, COUNT(*) as total FROM "PedidoVenda"
-UNION ALL SELECT 'produtos', COUNT(*) FROM "Produto"
-UNION ALL SELECT 'clientes', COUNT(*) FROM "Cliente"
-UNION ALL SELECT 'usuarios', COUNT(*) FROM "Usuario"
-UNION ALL SELECT 'fornecedores', COUNT(*) FROM "Fornecedor";
